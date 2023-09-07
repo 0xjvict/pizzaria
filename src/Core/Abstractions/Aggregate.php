f26d5a08e0046ecb334567957d89ca4e67aa62b5
0xjvict <jvmelo.dev@gmail.com>
2023-09-06 22:29:43 -0300

    fix: corrigindo abstração de Aggregate

--- src/Core/Abstractions/Aggregate.php
+++ src/Core/Abstractions/Aggregate.php
@@ -3,9 +3,14 @@
 namespace Core\Abstractions;
 
 use EventSauce\EventSourcing\AggregateAppliesKnownEvents;
+use EventSauce\EventSourcing\EventRecorder;
 use EventSauce\EventSourcing\EventSourcedAggregate;
 
 abstract class Aggregate implements EventSourcedAggregate
 {
     use AggregateAppliesKnownEvents;
+
+    public function __construct(private readonly EventRecorder $eventRecorder)
+    {
+    }
 }
