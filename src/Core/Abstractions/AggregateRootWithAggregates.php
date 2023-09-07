c7d258c2fe95e623a9c0b7e76063e6660951afb9
0xjvict <jvmelo.dev@gmail.com>
2023-09-06 23:00:37 -0300

    fix: deletando abstrações por causa de problemas de visibilidade

--- src/Core/Abstractions/AggregateRootWithAggregates.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Core\Abstractions;
-
-abstract class AggregateRootWithAggregates implements \EventSauce\EventSourcing\AggregateRoot
-{
-    use \EventSauce\EventSourcing\AggregateRootWithAggregates;
-}
