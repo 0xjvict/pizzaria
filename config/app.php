f2070d5795463e0624e7984e43f98868b4df2d92
0xjvict <jvmelo.dev@gmail.com>
2023-09-06 21:39:56 -0300

    feat: adicionando e configurando command bus no projeto

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Joselfonseca\LaravelTactician\Providers\LaravelTacticianServiceProvider::class,
     ])->toArray(),
 
     /*
